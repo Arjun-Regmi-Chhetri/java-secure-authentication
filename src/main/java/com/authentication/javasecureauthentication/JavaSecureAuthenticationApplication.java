7ccbcac43da85c77463ee21b6c6db89455e99a6c
Arjun-Regmi-Chhetri <arjunxetri100@gmail.com>
2026-02-15 09:46:27 +0545

    initial commit

---
+++ src/main/java/com/authentication/javasecureauthentication/JavaSecureAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.authentication.javasecureauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSecureAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSecureAuthenticationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/authentication/javasecureauthentication/JavaSecureAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authentication.javasecureauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSecureAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
